599abe10bf4748c175d0378aa58a80901b4d3032
hkportfolio <harveyjk86+hkportfolio@gmail.com>
2016-03-14 14:28:01 +0000

    Added some hard coded values so we've got something to display on the posts page

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        return [
+            {id:1, title:"Hello, World!", message:"Hello, World! This is the first post on Canary, it is hard-coded."},
+            {id:2, title:"Hello Again!", message:"This is the second post on Canary, it is also hard-coded."}
+        ];
+    }
 });
