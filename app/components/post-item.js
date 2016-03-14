542a80f3dd5bf2ce300050341844962ae5a54470
hkportfolio <harveyjk86+hkportfolio@gmail.com>
2016-03-14 16:00:08 +0000

    Added show and hide buttons for posts

--- app/components/post-item.js
+++ app/components/post-item.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    isMessageShowing: false,
+    actions: {
+        messageShow() {
+            this.set("isMessageShowing", true);
+        },
+        messageHide() {
+            this.set("isMessageShowing", false);
+        }
+    }
 });
